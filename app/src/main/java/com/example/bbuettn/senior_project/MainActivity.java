bb24e87bf1a18d2ae0e406c47829b2332f9325c5
BradB18 <bbuettn@siue.edu>
2018-03-18 18:16:04 -0500

    Login structure should be correct.

---
+++ app/src/main/java/com/example/bbuettn/senior_project/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bbuettn.senior_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+    /*@Override*/
+   /* protected void onCreate(Bundle savedInstanceState) {
+
+
+    }*/
+}
